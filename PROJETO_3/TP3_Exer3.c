ae84caae583c4f42f4c7ab9b44526591a22eaf15
Daniel Ventura <32223290+dventurb@users.noreply.github.com>
2024-11-28 13:18:55 +0000

    Update TP3_Exer3.c

--- PROJETO_3/TP3_Exer3.c
+++ PROJETO_3/TP3_Exer3.c
@@ -7,23 +7,24 @@
  *    Repetições necessárias: 
  *       2^0Hz = 1 segundo / 0.00125 segundo = 800 repetições
  *       2^1Hz = 0.5 segundo / 0.00125 segundo = 400 repetições
- *       2^2Hz = 0.25 segundo / 0.00125 segundo = 200 repetições 
+ *       2^2Hz = 0.25 segundo / 0.00125 segundo = 200 repetições
  *       2^3Hz = 0.125 segundo / 0.00125 segundo = 100 repetições
  *       2^4Hz = 0.0625 segundo / 0.00125 segundo = 50 repetições
  *       2^5Hz = 0.015625 segundo / 0.00125 segundo = 100 repetições
+ * 
  */
-
-
 
 #include "config.h"
 #include <xc.h>
 
+// Entradas e Saídas
 #define _XTAL_FREQ 8000000
 #define BOTAO_IN TRISBbits.RB0 = 1
 #define LED1_OUT TRISDbits.RD7 = 0  // Neste caso eu estou a utilizar o LED do PINO 7
 #define SEG_PINOS_C TRISC = 0b11000000
 #define SEG_PINOS_E TRISE = 0b100
 
+// Display de 7 Segmentos e o LED7
 #define SEG_A LATCbits.LATC0
 #define SEG_B LATCbits.LATC1
 #define SEG_C LATCbits.LATC2
@@ -32,23 +33,20 @@
 #define SEG_F LATCbits.LATC5
 #define SEG_G LATEbits.LATE0
 #define SEG_PD LATEbits.LATE1
-
 #define LED1 LATDbits.LATD7 
 
+// Funções
 void setup(void);
 void BCD_7SEG(int NUM);
 int tempo_desejado(int NUM);
 
+// Variáveis
 int repeticoes = 800; // Repeticoes necessarias para 2
 int NUM = 0;
-int ATUAL_BOTAO;
-int ANTES_BOTAO;
-
 
 void main(void){
     setup();
-    while(1){
-        
+    while(1){   
     }
     }
 
@@ -61,33 +59,38 @@ void setup(void){
     BCD_7SEG(NUM);
     repeticoes = tempo_desejado(NUM);
     
+    // Configurações do Timer e do Botão
     T0CONbits.T08BIT = 0;
     T0CONbits.T0CS = 0;
     T0CONbits.PSA = 0;
     T0CONbits.T0PS = 0b000;
-    TMR0 = 64910; // 1250 contagens
+    TMR0 = 64910; // 1250 contagens  (1 / ((8MHz / 4) * 2 * 1250 = 0.00125 segundo
     INTCONbits.GIE = 1;
     INTCONbits.PEIE = 1;
     INTCON2bits.TMR0IP = 1;
     INTCONbits.TMR0IF = 0;
     INTCONbits.TMR0IE = 1;
     T0CONbits.TMR0ON = 1;
+    INTCON2bits.INTEDG0 = 1;
+    INTCONbits.INT0IE = 1;
+    INTCONbits.INT0IF = 0;
 }
 
+// Interrupção do Timer e do Botão
 void __interrupt() rotina_isr(){
-    if (INTCONbits.TMR0IE && INTCONbits.TMR0IF){ // Verifica se houve uma interrupcao no Timer0
+    if (INTCONbits.TMR0IE && INTCONbits.TMR0IF){ // Verifica se houve uma Interrupção no Timer0
         if (repeticoes){       
-         repeticoes--; // Decrementa o numero de repeticoes necessarias
-        }else{  // Verifica se completou a repeticoes necessarias
-            LED1 = ~LED1; // Altera o estado do LED
-            repeticoes = tempo_desejado(NUM); // Retoma as repeticoes necessarias para a variavel, para repetir o ciclo. 
+         repeticoes--; // Decrementa o número de repetições
+        }else{  // Verifica se completou a repetições
+            LED1 = ~LED1; // Alterna o estado do LED
+            repeticoes = tempo_desejado(NUM); // Returna as repetições
         }
         TMR0 = 64910;
         INTCONbits.TMR0IF = 0; 
     }
     
     ATUAL_BOTAO = PORTBbits.RB0; 
-    if (ATUAL_BOTAO == 1 && ANTES_BOTAO == 0){
+    if (INTCONbits.INT0IF){
         NUM++;
         if (NUM > 5){
             NUM = 0;
@@ -95,7 +98,7 @@ void __interrupt() rotina_isr(){
         BCD_7SEG(NUM);
         repeticoes = tempo_desejado(NUM);
     }
-    ANTES_BOTAO = ATUAL_BOTAO;
+    INTCONbits.INT0IF = 0;
     }
 
 void BCD_7SEG(int NUM){
