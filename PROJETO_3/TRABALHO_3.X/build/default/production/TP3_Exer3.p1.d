b1bbba4fe3f9026823256e608eafd6586c87e981
Daniel Ventura <32223290+dventurb@users.noreply.github.com>
2025-03-11 20:00:51 +0000

    Delete PROJETO_3 directory

--- PROJETO_3/TRABALHO_3.X/build/default/production/TP3_Exer3.p1.d
+++
@@ -1,7 +0,0 @@
- build/default/production/TP3_Exer3.d  \
- build/default/production/TP3_Exer3.p1:  \
- TP3_Exer3.c  \
-/opt/microchip/mplabx/v6.20/packs/Microchip/PIC18Fxxxx_DFP/1.6.159/xc8/pic/include/proc/pic18f4580.h  \
-/opt/microchip/mplabx/v6.20/packs/Microchip/PIC18Fxxxx_DFP/1.6.159/xc8/pic/include/pic18.h  \
-config.h  \
-/opt/microchip/mplabx/v6.20/packs/Microchip/PIC18Fxxxx_DFP/1.6.159/xc8/pic/include/pic18_chip_select.h 
